10fa822749c71978ffc2a1ef9a5fa7553fbcafdb
Kevin Zhou <kevinzhou2222@gmail.com>
2020-05-20 22:43:11 -0400

    Change algorithm selection from Drawer to Select

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Graph from './Visualizer/Graph';
-import './App.css';
 
 function App() {
   return (
--- src/Visualizer/Graph.jsx
+++ src/Visualizer/Graph.jsx
@@ -3,12 +3,18 @@ import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
 import Slider from '@material-ui/core/Slider';
 import Typography from '@material-ui/core/Typography';
-import Drawer from '@material-ui/core/Drawer'
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
 
 import './Graph.css'
 
 
 const BAR_COLOR = '#81d4fa';
+
+const BUBBLE_SORT = 0;
+const INSERTION_SORT = 1;
+const MERGE_SORT = 2;
 
 export default class Graph extends React.Component {
     constructor(props) {
@@ -30,17 +36,16 @@ export default class Graph extends React.Component {
                             <Button variant='contained' color='secondary'>Sort!</Button>
                         </Box>     
                     </Box>
-                    <Box justifyContent='center'>
-                        <Button onClick={(event) => this.toggleDrawer(event, true)}>Choose Algorithm</Button>
-                        <Drawer open={this.state.drawerOpen} onClose={(event) => this.toggleDrawer(event, false)}>
-                            <Box width={250} display='flex' flexDirection='column'
-                            onClick={(event) => this.toggleDrawer(event, false)} 
-                            onKeyDown={(event) => this.toggleDrawer(event, false)}>
-                                <Button>Bubble Sort</Button>
-                                <Button>Insertion Sort</Button>
-                                <Button>Merge Sort</Button>
-                            </Box>
-                        </Drawer>
+                    <Box mx='auto' justifyContent='center'>
+                        <InputLabel id='algorithmLabel'>Algorithm</InputLabel>
+                        <Select
+                            labelId='algorithmLabel'
+                            defaultValue={BUBBLE_SORT}
+                        >
+                            <MenuItem value={BUBBLE_SORT}>Bubble Sort</MenuItem>
+                            <MenuItem value={INSERTION_SORT}>Insertion Sort</MenuItem>
+                            <MenuItem value={MERGE_SORT}>Merge Sort</MenuItem>
+                        </Select>
                     </Box>
                     {/*Slider to control how many numbers to sort*/}
                     <Box className='numberSlider' mx='auto' m={1}>
@@ -71,14 +76,6 @@ export default class Graph extends React.Component {
 
     componentDidMount() {
         this.resetArray();
-    }
-
-    toggleDrawer(event, open) {
-        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-            return;
-        }
-
-        this.setState({drawerOpen: open});
     }
 
     resetArray() {
